037d0838a0f315e1627e166f571b22bc49368976
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2021-08-29 01:23:31 +0300

    update

--- main.js
+++ main.js
@@ -1,43 +1,90 @@
 
-$('.multiple-items').slick({
-    // centerMode: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots:true,
-    variableWidth: true,
-    responsive: [
-                  {
-                     breakpoint: 1024,
-                     settings: {
-                        arrows: true,
-                        centerMode: true,
-                        centerPadding: '20px',
-                        slidesToShow: 2
-                     }
-                  },
-                  {
-                     breakpoint: 768,
-                     settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '10px',
-                        slidesToShow: 2
-                     }
-                  },
-                  {
-                     breakpoint: 320,
-                     settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '0px',
-                        slidesToShow: 1
-                     }
-                  }
-         
-               ]
-  });
+// $('.multiple-items').slick({
+//    //  centerMode: true,
+//    //  infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     dots:true,
+//     variableWidth: true,
+//     responsive:  [
+//       {
+//          breakpoint: 1024,
+//          settings: {
+//             arrows: false,
+//             centerMode: true,
+//             centerPadding: '20px',
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//          }
+//       },
+//       {
+//          breakpoint: 768,
+//          settings: {
+//             arrows: false,
+//             // centerMode: true,
+//             // centerPadding: '10px',
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//             dots:true,
+//          }
+//       },
+//       {
+//          breakpoint: 600,
+//          settings: {
+//             arrows: false,
+//             // centerMode: true,
+//             centerPadding: '0px',
+//             slidesToShow: 1
+//          }
+//       }
 
+//    ]
+//   });
+$(document).ready(function () {
+   $('.multiple-items').slick({
+      // centerMode: true,
+      variableWidth: true,
+      arrows: true,
+      dots: true,
+      // centerPadding: '30px',
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      // mobileFirst: false,
+      // waitForAnimate: false,
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               arrows: false,
+               // centerMode: true,
+               // centerPadding: '20px',
+               slidesToShow: 3,
+            }
+         },
+         {
+            breakpoint: 768,
+            settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: '10px',
+               slidesToShow: 3,
+               slidesToScroll: 3,
+            }
+         },
+         {
+            breakpoint: 600,
+            settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: '0px',
+               slidesToShow: 1
+            }
+         }
+
+      ]
+
+   });
+});
 
 
    
